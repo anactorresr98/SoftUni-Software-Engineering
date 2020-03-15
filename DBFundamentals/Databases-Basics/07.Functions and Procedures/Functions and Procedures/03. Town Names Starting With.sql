5d7197082cfbde56db8208b7944f8d6702ddbce1
anactorresr98 <55842468+anactorresr98@users.noreply.github.com>
2020-03-15 12:39:06 -0700

    Update 03. Town Names Starting With.sql

--- DBFundamentals/Databases-Basics/07.Functions and Procedures/Functions and Procedures/03. Town Names Starting With.sql
+++ DBFundamentals/Databases-Basics/07.Functions and Procedures/Functions and Procedures/03. Town Names Starting With.sql
@@ -1,7 +1,7 @@
-CREATE OR ALTER PROC usp_GetTownsStartingWith(@Text VARCHAR(50)) 
+CREATE OR ALTER PROC usp_GetTownsStartingWith(@Description VARCHAR(50)) 
          AS
 	 SELECT Name
 	   FROM Towns 
-	  WHERE LEFT(Name,LEN(@Text)) = @Text
+	  WHERE LEFT(Name,LEN(@Text)) = @Description
 
 EXEC dbo.usp_GetTownsStartingWith 'BELL'
